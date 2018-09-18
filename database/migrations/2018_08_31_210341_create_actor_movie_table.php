943df0f6550a6704a549131b7bb5988a0d8a1ffa
Radhikasachin <sach.radhika@gmail.com>
2018-09-18 14:44:32 +0200

    change classes of database tables

--- database/migrations/2018_08_31_210341_create_actor_movie_table.php
+++ database/migrations/2018_08_31_210341_create_actor_movie_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateActorMoviesTable extends Migration
+class CreateActorMovieTable extends Migration
 {
     /**
      * Run the migrations.
